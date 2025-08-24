d8aa49ef1128a3474cc9ba2bac3a033118bfe507
LucsKendi <lucaskazuma@hotmail.com>
2025-08-24 18:46:49 -0300

    feat: Exibir estrelas ou meia estrelas de acordo com o rating

--- src/components/QRating.tsx
+++ src/components/QRating.tsx
@@ -5,17 +5,25 @@ import QIconStar from "./QIconStar";
 interface QBadgeProps {
   rating: number;
 }
+const stars = Array(5).fill(0);
 
 const QRating: FC<QBadgeProps> = ({ rating }) => {
   return (
     <div className="flex items-center gap-2">
       <QText tag="span">{rating}</QText>
       <div className="flex items-center space-x-1 text-yellow-500">
-        <QIconStar />
-        <QIconStar />
-        <QIconStar />
-        <QIconStar />
-        <QIconStar half />
+        {stars.map((_, i) => {
+          if (i < Math.floor(rating)) {
+            // estrela cheia
+            return <QIconStar key={i} />;
+          } else if (i === Math.floor(rating) && rating % 1 !== 0) {
+            // meia estrela
+            return <QIconStar key={i} half />;
+          } else {
+            // estrela vazia
+            return <QIconStar key={i} empty />;
+          }
+        })}
       </div>
     </div>
   );
